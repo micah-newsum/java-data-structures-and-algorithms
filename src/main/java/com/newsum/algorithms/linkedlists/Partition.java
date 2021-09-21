2026883f802b2a1052469c5f7628140253bbeb2d
Micah Newsum <micahnew90@yahoo.com>
2021-09-20 20:22:36 -0700

    Create class with static method that partitions a linked list around a given value.

---
+++ src/main/java/com/newsum/algorithms/linkedlists/Partition.java
@@ -0,0 +1,28 @@
+package com.newsum.algorithms.linkedlists;
+
+import com.newsum.datastructures.LinkedListNode;
+
+public class Partition {
+    public static LinkedListNode partition(LinkedListNode node, int x){
+        LinkedListNode head = node;
+        LinkedListNode tail = node;
+
+        while (node != null){
+            LinkedListNode next = node.next;
+            if (node.data < x){
+                /* Insert node at head */
+                node.next = head;
+                head = node;
+            } else {
+                /* Insert node at tail */
+                tail.next = node;
+                tail = node;
+            }
+            node = next;
+        }
+        tail.next = null;
+
+        // The head has changed, so we need to return it to the user.
+        return head;
+    }
+}
