dcfff80455b6b62156576ddc8fcc2241630c5325
Micah Newsum <micahnew90@yahoo.com>
2021-09-21 17:44:13 -0700

    Implement insert method.

--- src/main/java/com/newsum/datastructures/Heap.java
+++ src/main/java/com/newsum/datastructures/Heap.java
@@ -11,4 +11,27 @@ public class Heap {
  public boolean isFull(){
      return size == heap.length;
  }
+
+ public void insert(int value){
+     if (isFull()){
+         throw new IndexOutOfBoundsException("Heap is full");
+     }
+
+     heap[size] = value;
+     fixHeapAbove(size);
+     size++;
+ }
+
+ private void fixHeapAbove(int index){
+     int newValue = heap[index];
+     while (index > 0 && newValue > heap[getParent(index)]){ // 0 index is root
+        heap[index] = heap[getParent(index)];
+        index = getParent(index);
+     }
+     heap[index] = newValue;
+ }
+
+ public int getParent(int index){
+     return (index - 1) / 2;
+ }
 }
