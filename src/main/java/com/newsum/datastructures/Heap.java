9902dea452b7ce67286f3eaae2578b4babeb5bd6
Micah Newsum <micahnew90@yahoo.com>
2021-09-21 18:33:56 -0700

    Add delete method.

--- src/main/java/com/newsum/datastructures/Heap.java
+++ src/main/java/com/newsum/datastructures/Heap.java
@@ -30,6 +30,56 @@ public class Heap {
      size++;
  }
 
+ public int delete(int index){
+     if (isEmpty()){
+         throw new IndexOutOfBoundsException("Heap is empty.");
+     }
+
+     int parent = getParent(index);
+     int deletedValue = heap[index];
+
+     // replace node with rightmost node
+     heap[index] = heap[size - 1];
+
+     if (index == 0 || heap[index] < heap[parent]){
+         fixHeapBelow(index, size - 1);
+     } else{
+         fixHeapAbove(index);
+     }
+
+     size--;
+
+     return deletedValue;
+ }
+
+ private void fixHeapBelow(int index, int lastHeapIndex){
+    int childToSwap;
+
+    while (index <= lastHeapIndex) {
+        int leftChild = getChild(index, true);
+        int rightChild = getChild(index, false);
+        if (leftChild <= lastHeapIndex){
+            if (rightChild > lastHeapIndex){
+                childToSwap = leftChild;
+            } else {
+                childToSwap = (heap[leftChild] > heap[rightChild] ? leftChild : rightChild);
+            }
+
+            if (heap[index] < heap[childToSwap]){
+                int temp = heap[index];
+                heap[index] = heap[childToSwap];
+                heap[childToSwap] = temp;
+            } else{
+                break;
+            }
+            index = childToSwap;
+        } else {
+            break;
+        }
+
+    }
+ }
+
  private void fixHeapAbove(int index){
      int newValue = heap[index];
      while (index > 0 && newValue > heap[getParent(index)]){ // 0 index is root
