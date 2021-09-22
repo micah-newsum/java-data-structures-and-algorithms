7c513867cb4708b6fef17636e785f7186c132c4b
Micah Newsum <micahnew90@yahoo.com>
2021-09-21 18:00:12 -0700

    Add isEmpty method.

--- src/main/java/com/newsum/datastructures/Heap.java
+++ src/main/java/com/newsum/datastructures/Heap.java
@@ -12,6 +12,10 @@ public class Heap {
      return size == heap.length;
  }
 
+ public boolean isEmpty(){
+     return size == 0;
+ }
+
  public void insert(int value){
      if (isFull()){
          throw new IndexOutOfBoundsException("Heap is full");
